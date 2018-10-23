272e119107915d8387c7b39c93b8ae0ae220f114
Alexandros Kotsis Giannarakis <Alexandros.Giannarakis@Peoplecert.onmicrosoft.com>
2018-10-23 20:39:05 +0300

    Whatever.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float weight = 80; // this is a comment
+            float height = 1.8f;
+            float bmi = (weight / (height * height));
+            // Console.WriteLine print something to the console
+            Console.WriteLine($"BMI is: {bmi}");
+            Console.Read();
+        }
+    }
+}
